bfd171ff2c7c5c74d95180115f0f361fdf7ea483
macqueen0987 <macqueen0987@gmail.com>
2025-05-08 16:03:53 +0900

    piece 를 arraylist  로 변경

--- src/main/logic/BoardSlot.java
+++ src/main/logic/BoardSlot.java
@@ -1,22 +1,31 @@
 package main.logic;
 
+import java.util.ArrayList;
+
 public class BoardSlot extends Entity {
     private BoardSlot[] next;
     private BoardSlot[] prev;
-    private Piece piece;
+//    private Piece piece;
+    private ArrayList<Piece> piece;
     public final int num;
 
     public BoardSlot(int num, int polygon) {
         this.num = num;
         this.next = new BoardSlot[(polygon/2) + 1];
         this.prev = new BoardSlot[(polygon/2) + 1];
+        this.piece = new ArrayList<>();
     }
 
     public void setNext(int index, BoardSlot slot) { next[index] = slot; }
     public void setPrev(int index, BoardSlot slot) { prev[index] = slot; }
     public BoardSlot[] getNext() { return next; }
     public BoardSlot[] getPrev() { return prev; }
-    public void addPiece(Piece piece) { this.piece = piece; }
-    public void removePiece() { this.piece = null; }
-    public Piece getPiece() { return piece; }
+    public void setPiece(Piece piece) { this.piece = new ArrayList<>(); this.piece.add(piece); }
+    public void addPiece(Piece piece) { this.piece.add(piece); }
+    public void removePiece(Piece piece) { this.piece.remove(piece); }
+    public Piece getPiece() {
+        if (piece.isEmpty()) return null;
+        return piece.getFirst();
+    }
+    public ArrayList<Piece> getPieces() { return piece; }
 }
